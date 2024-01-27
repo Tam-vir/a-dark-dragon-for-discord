a3f7344d291be1cf39ba494b3c9476d95ba88671
Tam-vir <93982408+Tam-vir@users.noreply.github.com>
2024-01-27 17:54:36 +0000

    new map

--- utils/generateMap.js
+++ utils/generateMap.js
@@ -1,32 +1,30 @@
 const generateMap = () => {
+  /** 
+   grass: 0,
+   player: 1,
+   water: 5,
+   treasure: 100
+  
+  */
   let world = []
-  for (let i = 0; i < 500; i++){
+  for (let i = 0; i < 50; i++){
     world.push([]);
-    for (let j = 0; j < 500; j++){
-      world[i].push({
-        type: 'grass'
-      });
+    for (let j = 0; j < 50; j++){
+      world[i].push(0);
     }
   }
-  world[250][250] = {
-    type: 'player'
-  };
-  for(let i = 400; i < 408; i++){
-    for(let j = 400; j < 408; j++){
-      world[i][j] = {
-        type: 'water'
-      };
+  world[25][25] = 1;
+  for(let i = 40; i < 48; i++){
+    for(let j = 40; j < 48; j++){
+      world[i][j] = 5;
     }
   }
   let treasuresSet = 0;
   function setTreasure(){
-    let x = Math.floor(Math.random() * 500);
-    let y = Math.floor(Math.random() * 500);
-    if (world[x][y].type === 'grass'){
-      world[x][y] = {
-        type: 'treasure',
-        value: Math.floor(Math.random() * 70) + 30
-      };
+    let x = Math.floor(Math.random() * 50);
+    let y = Math.floor(Math.random() * 50);
+    if (world[x][y] === 0){
+      world[x][y] = 100;
       treasuresSet++;
     }
   }
