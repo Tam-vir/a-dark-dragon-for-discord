86d2c3e89857cee3f994184f0e2280633211f289
Tam-vir <93982408+Tam-vir@users.noreply.github.com>
2024-01-27 19:47:31 +0600

    Update inventory.js

--- commands/misc/inventory.js
+++ commands/misc/inventory.js
@@ -5,19 +5,20 @@ module.exports = {
   callback: async (client, interaction) => {
      const itr = await interaction;
     const query = {
-      userId: itr.user.id,
+      userId: interaction.user.id,
     };
     const player = await Player.findOne(query);
     if (player){
-      await itr.reply(`Loading...`);
+      await interaction.reply(`Loading...`);
       let inventory = player.inventory;
       let message = `Inventory of ${interaction.user.username}: \n${player.inventory.map(item => `${item.type}: ${item.amount}`).join('\n')}`
-      await itr.editReply(message);
+      await interaction.editReply(message);
+      return;
     }
     else{
       
       await interaction.reply("Use the `/thstart` command to start as a hunter !");
-     
+       return;
     }
   },
 };
