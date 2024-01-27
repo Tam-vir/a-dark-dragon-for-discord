442c6db76e26b0b87567be645c1eeb48e8565536
Tam-vir <93982408+Tam-vir@users.noreply.github.com>
2024-01-27 19:44:08 +0600

    Update inventory.js

--- commands/misc/inventory.js
+++ commands/misc/inventory.js
@@ -3,16 +3,16 @@ module.exports = {
   name: 'inventory',
   description: 'Check your inventory',
   callback: async (client, interaction) => {
- 
+     const itr = await interaction;
     const query = {
-      userId: interaction.user.id,
+      userId: itr.user.id,
     };
     const player = await Player.findOne(query);
     if (player){
-      await interaction.reply(`Loading...`);
+      await itr.reply(`Loading...`);
       let inventory = player.inventory;
       let message = `Inventory of ${interaction.user.username}: \n${player.inventory.map(item => `${item.type}: ${item.amount}`).join('\n')}`
-      await interaction.editReply(message);
+      await itr.editReply(message);
     }
     else{
       
